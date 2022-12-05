18cb0c38597f2fc6297bd27ca7f7b51a37cbf81d
mathew bushuru <bushuruwafulamathew@gmail.com>
2022-12-05 01:53:31 -0800

    consolidate all functions under a single operate function

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+      break;
+    default:
+      console.log(`${operator} operation not available`);
+      break;
+  }
+}
+
+console.log(operate("/",2,3))
